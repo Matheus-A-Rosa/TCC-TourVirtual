5a01f40bdd4c9535a31b09b29929455749f7f423
yuriranieri <yurifranieri@gmail.com>
2022-08-07 15:28:04 -0300

    adicionando projeto backend

---
+++ backend/pinkeri-tours/src/main/java/br/com/pinkeritours/PinkeriToursApplication.java
@@ -0,0 +1,13 @@
+package br.com.pinkeritours;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PinkeriToursApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PinkeriToursApplication.class, args);
+	}
+
+}
