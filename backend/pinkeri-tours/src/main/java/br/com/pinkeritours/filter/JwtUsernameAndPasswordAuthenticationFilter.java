5ce4623b9cd335c7ac5a0920c8e10b334a3402ee
Furtado Ranieri <yifi@gft.com>
2022-09-16 17:53:04 -0300

    adicionando inicio do jwt

---
+++ backend/pinkeri-tours/src/main/java/br/com/pinkeritours/filter/JwtUsernameAndPasswordAuthenticationFilter.java
@@ -0,0 +1,36 @@
+package br.com.pinkeritours.filter;
+
+import java.io.IOException;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@RequiredArgsConstructor
+public class JwtUsernameAndPasswordAuthenticationFilter extends
+    UsernamePasswordAuthenticationFilter {
+
+  private final AuthenticationManager authenticationManager;
+
+  @Override
+  public Authentication attemptAuthentication(HttpServletRequest request,
+      HttpServletResponse response) throws AuthenticationException {
+    String username = request.getParameter("username");
+    String password = request.getParameter("password");
+    var authenticationToken = new UsernamePasswordAuthenticationToken(username, password);
+    return authenticationManager.authenticate(authenticationToken);
+  }
+
+  @Override
+  protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response,
+      FilterChain chain, Authentication authResult) throws IOException, ServletException {
+
+    super.successfulAuthentication(request, response, chain, authResult);
+  }
+}
--- backend/pinkeri-tours/src/main/java/br/com/pinkeritours/security/WebSecurityConfig.java
+++ backend/pinkeri-tours/src/main/java/br/com/pinkeritours/security/WebSecurityConfig.java
@@ -3,18 +3,33 @@ package br.com.pinkeritours.security;
 import static org.springframework.http.HttpMethod.GET;
 import static org.springframework.http.HttpMethod.POST;
 
+import br.com.pinkeritours.filter.JwtUsernameAndPasswordAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class WebSecurityConfig {
 
   @Bean
+  public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig)
+      throws Exception {
+    return authConfig.getAuthenticationManager();
+  }
+
+  @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     http
         .csrf().disable()
+//        .sessionManagement()
+//        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+//        .and()
+//        .addFilter(new JwtUsernameAndPasswordAuthenticationFilter(authenticationManager()))
+//        .addFilterAfter(null, null)
         .httpBasic()
         .and()
         .authorizeHttpRequests()
