10eb43f333ec3579ba16c9d3f2cf0097a8237644
yuriranieri <yurifranieri@gmail.com>
2022-11-23 19:34:25 -0300

    adiconando host da aws

--- frontend/tour-virtual/src/services/api.js
+++ frontend/tour-virtual/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8080/v1/imoveis',
+    baseURL: 'http://18.209.60.81:8080/v1/imoveis',
+    // baseURL: 'http://localhost:8080/v1/imoveis',
 })
 
 
